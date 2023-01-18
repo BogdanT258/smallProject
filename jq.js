7ba797797c47df2b081665f5e807007ab792144c
Bogdan Trivic <62573562+BogdanT258@users.noreply.github.com>
2023-01-18 02:27:35 +0100

    Jquery first app, simple toDo list

--- jq.js
+++ jq.js
@@ -26,10 +26,43 @@ $("document").ready(function(){
    // $("#btn").dblclick(function(){
    //    $("#p-event").css("color", "blue");
    // })
-   $("#p-event").mouseenter(function(){
-      $("#p-event").css("font-size","24px");
+   // $("#p-event").mouseenter(function(){
+   //    $("#p-event").css("font-size","24px");
+   // });
+   // $("#p-event").mouseleave(function(){
+   //    $("#p-event").css("font-size","16px");
+   // });
+   // $("#p-event").hover(func1, func2)
+   // function func1(){
+   //    $("#p-event").css("font-size","24px");
+   // }
+   // function func2(){
+   //    $("#p-event").css("font-size","16px");
+   // }
+   $("#btn1").click(function(){
+      $("#img1").hide(2000, "swing");
    })
-   $("#p-event").mouseleave(function(){
-      $("#p-event").css("font-size","16px");
+   $("#btn2").click(function(){
+      $("#img1").show(2000);
+   })
+   $("#btn3").click(function(){
+      $("#img1").toggle(500);
    })
 });
+
+$("document").ready(function(){
+   $('input').on('keypress',function(e){
+      if (e.which == 13) {
+         var todo = $(this).val();
+         $('ul').append('<li>' + todo + '<i class="icon fa fa-check"></i><i class="icon fa fa-trash"></li>');
+         $(this).val('');        
+      }
+   })
+   $('ul').on('click','.fa-check',function(){
+      $(this).parent('li').toggleClass('checked');
+      console.log("here");
+   })
+   $('ul').on('click','.fa-trash',function(){
+      $(this).parent('li').fadeOut(200);
+   })
+})
